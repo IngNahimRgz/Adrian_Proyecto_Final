70ee9908a2601565cc435c550bc74cf0501c82d0
IngNahimRgz <nahimrgz@ingenieros.com>
2018-05-27 10:11:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/adrian/adrian_proyecto_final/fragments/MainFragment.java
@@ -0,0 +1,43 @@
+package com.example.adrian.adrian_proyecto_final.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.adrian.adrian_proyecto_final.R;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    TextView txtVw;
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        txtVw = (TextView)rootView.findViewById(R.id.textView) ;
+
+
+
+
+        return rootView;
+    }
+}
